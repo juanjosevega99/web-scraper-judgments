8c6e34c9d9bcd398bf0db98d7f333ea62f514872
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-12-05 15:43:15 -0500

    add file number to get sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
+from scraper import scrape_website
 
 app = Flask(__name__)
 
@@ -9,5 +10,16 @@ def index():
     return jsonify({"Choo Choo": "Welcome to your web scrapper"})
 
 
+@app.route('/scrape', methods=['GET'])
+def scrape():
+    file_number = request.args.get('file_number')
+    if not file_number:
+        return jsonify({"error": "Missing URL parameter", "status": "failed"}), 400
+
+    result = scrape_website(file_number)
+    return jsonify(result)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(debug=True, port=os.getenv("PORT", default=8080))
